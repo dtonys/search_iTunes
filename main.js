fed757ea755f296d196d1c4e7e6018f28921271d
Tony Schwartz <dtonys@gmail.com>
2013-07-02 11:30:00 -0700

    added finished flag

--- main.js
+++ main.js
@@ -10,20 +10,23 @@ $(document).ready(function(e){
   var dataArray = [];
   var resultList = [];
   var resultHtml = null;
+  var finished = true;
   
   //send request whenever user types key
   $('.search-wrap').on('keyup', function(e){
+    if(!finished) return true;
+    finished = false;
     var val = $('#search').val();
     if(val){
       $.ajax({
         url: "http://itunes.apple.com/search?term="+val,
         dataType: 'JSONP'
       }).done(function(data){
+        finished = true;
         if(!data){
           alert('no data');
           return;
         }
-        console.log(data);
         $count.text(data.resultCount);
         $resultHtml = null;
         $results.empty();
@@ -39,14 +42,15 @@ $(document).ready(function(e){
       })
       .fail(function(data){
         alert(data);
-        console.log(data);
       });
     }
   });
   
   //When user clicks image, display all metadata
   $(document).on('click', '.list-template', function(e){
-    alert(JSON.stringify(dataArray[$(this).index()]), undefined, 2);
+    //alert(JSON.stringify(dataArray[$(this).index()]), undefined, 2);
+    alert(JSON.stringify(resultList[$(this).index()].artist), undefined, 2);
+    return false;
   });
 });
 
@@ -56,6 +60,6 @@ function result(data){
   this.artist = data.artistName
   this.image = data.artworkUrl100;
   this.url = data.artistViewUrl;
-  this.html = '<li title="'+data.artistName+'><a href="'+data.artistViewUrl+
+  this.html = '<li class="list-template" title="'+data.artistName+'"><a href="'+data.artistViewUrl+
               '"><img src="'+this.image+'" /></a></li>';
 }
